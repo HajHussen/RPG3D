d808026c27f0716ad25bcce9d659c480007ce63c
AmirHossein M <amirgooli.maali@gmail.com>
2023-08-29 12:46:04 +0330

    Class organization

---
+++ RPG3D/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+namespace RPG.Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] GameObject target;
+
+
+        void LateUpdate()
+        {
+            gameObject.transform.position = target.transform.position;
+        }
+    }
+}
+
