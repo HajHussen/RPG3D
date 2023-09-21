1b7effa08f8f493e61f8e45fe9d81cf875073015
AmirHossein M <amirgooli.maali@gmail.com>
2023-09-21 17:23:55 +0330

    add fader

---
+++ RPG3D/Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3);
+            yield return FadeIn(1);
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
