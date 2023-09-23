08462b031b9833835a14d6a5c4779132bba4d553
AmirHossein M <amirgooli.maali@gmail.com>
2023-09-23 12:52:34 +0330

    implement save system

--- RPG3D/Assets/Scripts/Movement/Mover.cs
+++ RPG3D/Assets/Scripts/Movement/Mover.cs
@@ -1,11 +1,12 @@
 
 using RPG.Core;
+using RPG.Saving;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
 
         [SerializeField] Transform target;
@@ -46,6 +47,19 @@ namespace RPG.Movement
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("ForwardMoveSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 
---
+++ RPG3D/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+using System;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFolder = "save";
+
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFolder);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSaveFolder);
+        }
+    }
+}
